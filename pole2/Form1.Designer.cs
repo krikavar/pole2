daa857a8453291c643cd966c63816fad25dddf5e
Radek Křikava <krikavar.03@spst.cz>
2021-12-12 21:15:03 +0100

    Přidejte soubory projektu.

---
+++ pole2/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace pole2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelavr = new System.Windows.Forms.Label();
+            this.label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 404);
+            this.listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox1.Location = new System.Drawing.Point(142, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(142, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Výpočet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelavr
+            // 
+            this.labelavr.AutoSize = true;
+            this.labelavr.Location = new System.Drawing.Point(139, 77);
+            this.labelavr.Name = "labelavr";
+            this.labelavr.Size = new System.Drawing.Size(0, 17);
+            this.labelavr.TabIndex = 3;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(139, 113);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(0, 17);
+            this.label.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(319, 425);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.labelavr);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "POLE 2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelavr;
+        private System.Windows.Forms.Label label;
+    }
+}
+
---
+++ pole2/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pole2
+{
+    public partial class Form1 : Form
+    {
+        int[] poleA;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+			Random random = new Random();
+			listBox1.Items.Clear();
+			try
+			{
+				int n = int.Parse(textBox1.Text);
+				poleA = new int[n];
+				double avr;
+				double max = -1000;
+				for (int i = 0; i < n; i++)
+				{
+					poleA[i] = random.Next(-1000, 1000);
+					listBox1.Items.Add(poleA[i]);
+				}
+				avr = poleA.Average();
+				labelavr.Text = "Prumer je: " + avr.ToString();
+				foreach (double c in poleA)
+				{
+					if (c <= avr && c > max) { 
+						max = c; 
+					}
+				}
+				label.Text = "Max z prvku < || == je: " + max.ToString();
+			}
+			catch
+			{
+				MessageBox.Show("CHYBA!");
+			}
+		}
+    }
+}
